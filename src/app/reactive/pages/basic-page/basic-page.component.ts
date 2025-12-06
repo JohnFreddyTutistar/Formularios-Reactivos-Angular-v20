7cba5378e0116ea0dff8fe0831566d7ba497268e
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-12-06 01:10:05 -0500

    trabajando con validaciones en formularios reactivos

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,21 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'basic-page',
-  imports: [ JsonPipe ],
+  imports: [ JsonPipe, ReactiveFormsModule ],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css',
 })
 export class BasicPageComponent {
+
+  fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    name: ['', [Validators.required]],
+    price: [0],
+    inStorage: [0],
+  })
 
 }
